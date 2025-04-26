e2a1a469ba42e794e03f980f4693c68fb2a60bc3
PPYYQQ <945505411@qq.com>
2025-04-26 15:40:50 +0800

    image path

--- static/script.js
+++ static/script.js
@@ -12,7 +12,7 @@ const data = [
       homepage: "https://pku.ai/author/yichao-zhou/"
     },
     {
-      name: "Hongjie Li",
+      name: "hongjieli",
       score: 2,
       email: "lihongjie@stu.pku.edu.cn",
       homepage: "https://awfuact.github.io/"
